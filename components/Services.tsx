e61aaa5633ec741ca6ec6a4bfedf2e2f0cfbad7f
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-01-20 01:13:28 +0000

    Update css

--- components/Services.tsx
+++ components/Services.tsx
@@ -62,7 +62,7 @@ export default function Services() {
 
                 {/* Cards grid - this is the critical part */}
                 {/* Cards grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 items-stretch">
                     {services.map((service, index) => (
                         <motion.div
                             key={service.id}
